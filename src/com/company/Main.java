0199ab90671e74040de7c1bbe861ed0eb7d34b01
Omirr85 <david.vlaminck@telenet.be>
2018-08-23 13:45:41 +0200

    2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import main.java.InheritTest;
+import main.java.PostgreSqlExample;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
@@ -8,7 +9,7 @@ import java.security.GeneralSecurityException;
 public class Main {
 
     public static void main(String[] args) throws IOException, GeneralSecurityException {
-        InheritTest c = new InheritTest();
-        c.main();
+        PostgreSqlExample  c = new PostgreSqlExample();
+        c.main(new String[]{});
     }
 }
