a297e6befb3fbbf6525c7421ed25e561773969e9
David Vlaminck <vlaminda@BELFLA.BE>
2018-08-23 12:23:07 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import main.java.InheritTest;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, GeneralSecurityException {
+        InheritTest c = new InheritTest();
+        c.main();
+    }
+}
